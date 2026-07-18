1fdb2d357dbc7813bb9d416b87fbbebe8f478bdc
SaraVieira <hey@iamsaravieira.com>
2026-07-18 17:26:57 +0100

    add stamina colors

--- scripts/time_of_day_label.gd
+++ scripts/time_of_day_label.gd
@@ -1,14 +1,37 @@
 extends Node
 
 const DEFAULT_SIZE = 2.833
+@onready var green = $Stamina/Green
+@onready var yellow = $Stamina/Yellow
+@onready var red = $Stamina/Red
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameState.player_stamina_changed.connect(_on_player_stamina_changed)
 
 func _on_player_stamina_changed(stamina: int) -> void:
-	print(stamina)
-	$TileMapLayer2/Sprite2D.scale.x = DEFAULT_SIZE * (stamina / 100.0)
+	var size = DEFAULT_SIZE * (stamina / 100.0)
+	var sprites = [green, yellow, red]
+
+	for n in sprites:
+		n.scale.x = size
+
+	if stamina > 60:
+		green.visible = true
+		yellow.visible = false
+		red.visible = false
+	elif stamina > 30:
+		green.visible = false
+		yellow.visible = true
+		red.visible = false
+	else:
+		green.visible = false
+		yellow.visible = false
+		red.visible = true
+
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
