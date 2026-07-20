4251860658ffa332ab766c8cd521ed4f5c3f8aa8
SaraVieira <hey@iamsaravieira.com>
2026-07-20 12:20:37 +0100

    reorganize

--- autoload/gameState.gd
+++ autoload/gameState.gd
@@ -11,8 +11,8 @@ signal night_started
 const DAY_START_HOUR := 6
 const NIGHT_START_HOUR := 18
 var LEVELS = {
-	  "PARK": { "scene": preload("res://scenes/park.tscn"), "fade": Color("2e5e40") },
-	  "CITY": { "scene": preload("res://scenes/city.tscn"), "fade": Color("3d3d4e") },
+	  "PARK": { "scene": preload("res://levels/park/park.tscn"), "fade": Color("2e5e40") },
+	  "CITY": { "scene": preload("res://levels/city/city.tscn"), "fade": Color("3d3d4e") },
 }
 var currentLevel = LEVELS["CITY"]
 var player_stamina := 100
--- ui/stamina/stamina_bar.gd
+++ ui/stamina/stamina_bar.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 const DEFAULT_SIZE = 2.833
 @onready var green = $Stamina/Green
@@ -29,27 +29,3 @@ func _on_player_stamina_changed(stamina: int) -> void:
 		green.visible = false
 		yellow.visible = false
 		red.visible = true
-
-
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var minutes = (int((GameState.TIME - int(GameState.TIME)) * 60))
-
-	var in_game_hours = (int(GameState.TIME) % 24)
-	var in_game_minutes = int((GameState.TIME - int(GameState.TIME)) * 60)
-	var in_game_hours_12 = ((in_game_hours + 11) % 12 + 1)
-	var am_pm = "AM" if in_game_hours < 12 else "PM"
-
-
-	var str_hours = str(in_game_hours_12)
-	var str_minutes = str(in_game_minutes)
-
-	if int(in_game_hours_12) < 10:
-		str_hours = "0" + str_hours
-	if int(minutes) < 10:
-		str_minutes = "0" + str_minutes
-
-
-	$Label.text = str_hours + " : " + str_minutes +" " + am_pm
