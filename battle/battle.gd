b678c7467159eec305e1a5571c2e151aa94463ee
Sara Vieira <hey@iamsaravieira.com>
2026-07-25 19:21:10 +0100

    connect disabled

--- battle/battle.gd
+++ battle/battle.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var cat: Node2D = $cat
 
 var ACTIONS := ["pet", "hand", "treats", "sound", "sit", "photo"]
 @onready var STATES = {
@@ -12,37 +13,70 @@ var ACTIONS := ["pet", "hand", "treats", "sound", "sit", "photo"]
 	"photo": { "instance": null, "cooldown": 0.0, "ui": preload("res://battle/photo/photo.tscn") },
 }
 
+var current_action := ""
 
+var trust := 0
+
+
+func _show_choose() -> void:
+	var inst = STATES["choose"]["ui"].instantiate()
+	STATES["choose"]["instance"] = inst
+	add_child(inst)
+	inst.setup(cat.data, trust)
 
 func _ready() -> void:
-	STATES["choose"]["instance"] = STATES["choose"].ui.instantiate()
-	add_child(STATES["choose"]["instance"])
+	trust = cat.data.starting_trust
+
 	BattleSignals.connect("on_menu_option_selected", _on_battle_choose_option_selected)
-	BattleSignals.connect("on_pet_result", _on_pet_on_pet_attack)
-	BattleSignals.connect("on_sound_result", _on_sound_ui_on_sound_attack)
+	BattleSignals.connect("on_pet_result", _on_pet_result)
+	BattleSignals.connect("on_sound_result", _on_sound_result)
+
+	_show_choose()
+
 
 func _on_battle_choose_option_selected(option_action: String) -> void:
-	STATES[option_action]["instance"] = STATES[option_action]["ui"].instantiate()
-	add_child(STATES[option_action]["instance"])
+	current_action = option_action
+
+	var inst = STATES[option_action]["ui"].instantiate()
+	STATES[option_action]["instance"] = inst
+	add_child(inst)
+
 	STATES["choose"]["instance"].queue_free()
 	STATES["choose"]["instance"] = null
 
+
 func change_scene_to_choose() -> void:
-	var current_action = ""
-	print(current_action)
-	for action in STATES.keys():
-		if STATES[action]["instance"] != null:
-			current_action = action
-			break
+	if current_action == "":
+		return
 
 	STATES[current_action]["instance"].queue_free()
-	STATES["choose"]["instance"] = STATES["choose"]["ui"].instantiate()
-	add_child(STATES["choose"]["instance"])
+	STATES[current_action]["instance"] = null
+	current_action = ""
+
+	_show_choose()
 
 
-func _on_pet_on_pet_attack(hit_success: bool) -> void:
+
+func _apply_result(action: String, hit_success: bool) -> void:
+	var delta := 0
+
+	if action == cat.data.hates:
+		# Fires on a hit too. This one can't be out-played.
+		delta = cat.data.hated_penalty()
+	elif hit_success:
+		delta = cat.data.gain_for(action)
+	else:
+		delta = cat.data.penalty_for(action)
+
+	trust = clampi(trust + delta, cat.data.flee_threshold, 100)
+	print("%s: %s %+d -> trust %d" % [cat.data.display_name, action, delta, trust])
+
+
+func _on_pet_result(hit_success: bool) -> void:
+	_apply_result("pet", hit_success)
 	change_scene_to_choose()
 
 
-func _on_sound_ui_on_sound_attack(hit_success: bool) -> void:
+func _on_sound_result(hit_success: bool) -> void:
+	_apply_result("sound", hit_success)
 	change_scene_to_choose()
--- battle/choose/choose.gd
+++ battle/choose/choose.gd
@@ -5,6 +5,10 @@ const COLS := 3
 var active_index := 0
 var option_keys
 
+## action -> bool. Filled in by setup(); locked options fade and are skipped.
+var locked := {}
+
+
 func _ready() -> void:
 	var parent = get_parent()
 	if "ACTIONS" in parent:
@@ -15,23 +19,45 @@ func _ready() -> void:
 	update_active_option()
 
 
+## Called by battle.gd right after add_child(), every time the menu comes back.
+func setup(data: CatData, current_trust: int) -> void:
+	for action in option_keys:
+		locked[action] = not data.is_available(action, current_trust)
+		_container(action).modulate.a = 0.35 if locked[action] else 1.0
+
+	_move(0)
+
+
+func _is_locked(action: String) -> bool:
+	return locked.get(action, false)
+
+
+func _move(step: int) -> void:
+	var n: int = option_keys.size()
+	var dir := -1 if step < 0 else 1
+	var i: int = (active_index + step + n * 2) % n
+
+	for _attempt in n:
+		if not _is_locked(option_keys[i]):
+			active_index = i
+			update_active_option()
+			return
+		i = (i + dir + n) % n
+
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("enter"):
+	if Input.is_action_just_pressed("enter") and not _is_locked(option_keys[active_index]):
 		BattleSignals.emit_signal("on_menu_option_selected", option_keys[active_index])
 
 	if Input.is_action_just_pressed("left"):
-		active_index = (active_index - 1 + option_keys.size()) % option_keys.size()
-		update_active_option()
+		_move(-1)
 	elif Input.is_action_just_pressed("right"):
-		active_index = (active_index + 1) % option_keys.size()
-		update_active_option()
+		_move(1)
 
 	if Input.is_action_just_pressed("up"):
-		active_index = (active_index - COLS + option_keys.size()) % option_keys.size()
-		update_active_option()
+		_move(-COLS)
 	elif Input.is_action_just_pressed("down"):
-		active_index = (active_index + COLS) % option_keys.size()
-		update_active_option()
+		_move(COLS)
 
 
 func _container(action: String) -> HBoxContainer:
